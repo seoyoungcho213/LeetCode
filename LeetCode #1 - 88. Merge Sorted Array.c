6aa13db7030ae215f565280c536a864ed8642395
seoyoungcho213 <70723003+seoyoungcho213@users.noreply.github.com>
2023-06-29 23:39:20 -0400

    Update LeetCode #1 - 88. Merge Sorted Array.c

--- LeetCode #1 - 88. Merge Sorted Array.c
+++ LeetCode #1 - 88. Merge Sorted Array.c
@@ -1,6 +1,5 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
     int temp = 10;
-
     for(int i = m; i < (m+n); i++) {nums1[i] = nums2[i-m];}
     for(int i = (m+n-1); i >= 0; i--) {
         if((nums1[i] > temp) && i < (m+n-1)) {
